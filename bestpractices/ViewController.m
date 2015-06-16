c3c570d909a51993d9ddc613df9ca1ff863e2de3
SpurTree <spurtree@SpurTrees-MacBook.local>
2015-06-16 11:49:20 +0530

    Initial Commit

---
+++ bestpractices/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  bestpractices
+//
+//  Created by SpurTree on 16/06/15.
+//  Copyright (c) 2015 SpurTree. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ bestpractices/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  bestpractices
+//
+//  Created by SpurTree on 16/06/15.
+//  Copyright (c) 2015 SpurTree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
